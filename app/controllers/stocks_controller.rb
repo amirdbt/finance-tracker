908ae6051dc7e53f4e20a5051e693d4f37795950
Ahmed Dambatta Hassan <hassanamir643@yahoo.com>
2021-01-18 01:13:35 +0100

    Added JS responses to invalid search results

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -6,14 +6,17 @@ class StocksController < ApplicationController
         respond_to do |format|
           format.js { render partial: 'users/results' }
         end
-        # render 'users/my_portfolio'
       else
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = 'Please enter a valid symbol to search'
+          format.js { render partial: 'users/results' }
+        end
       end
     else
-      flash[:alert] = 'Please enter a symbol to search'
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = 'Please enter a symbol to search'
+        format.js { render partial: 'users/results' }
+      end
     end
   end
 end
