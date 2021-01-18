c4ec8b7206a618aceec6dec2f0d966d51d0f26de
Ahmed Dambatta Hassan <hassanamir643@yahoo.com>
2021-01-18 01:04:05 +0100

    Added secure API key, build stock up lookup form, dealt with invalid search results, used Ajax for form submission

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,10 +2,16 @@ class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
-      # secret_token: 'sk_a3a41e9812194499936da13157bd3ceb',
-      endpoint: 'https://cloud.iexapis.com/v1'
+      publishable_token: 'Tpk_772a6fcd897b4eac800879b3c2991f4a',
+      # publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+      secret_token: 'Tsk_ed169eb46ca5462092d101b53f8edec7',
+      endpoint: 'https://sandbox.iexapis.com/stable'
     )
-    client.price(ticker_symbol)
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue StandardError => exception
+      nil
+    end
+
   end
 end
