4d9a04bff7a5f883cacdb8ee7e7c73c7cd4eb29c
Ahmed Dambatta Hassan <hassanamir643@yahoo.com>
2021-01-18 19:43:55 +0100

    Added a list view

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,7 @@
 class Stock < ApplicationRecord
-
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  validates :name, :ticker, presence: true
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: 'Tpk_772a6fcd897b4eac800879b3c2991f4a',
